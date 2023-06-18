2ff58ac5cb0a22c9553c43757cea7a527a179bdf
Homero José <96498455+rox651@users.noreply.github.com>
2023-06-18 17:36:08 -0500

    fixing text contact

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -4,7 +4,7 @@ import SocialMedia from "./SocialMedia";
 
 const Contact = () => {
    return (
-      <section className="grid min-h-[300px]  content-center items-center gap-10 py-5 lg:min-h-[700px]">
+      <section className="grid min-h-[300px]  content-center items-center gap-10 overflow-hidden py-5 lg:min-h-[700px]">
          <h3 className="mx-auto  grid   font-dela text-3xl uppercase lg:text-6xl xl:text-7xl 2xl:text-8xl">
             <TextAnimate text="Work" />
             <TextAnimate text="Together" />
