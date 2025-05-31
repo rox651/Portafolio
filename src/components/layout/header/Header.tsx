840391694b30b20697f40a10997595bfac74498e
homero <homerojose69@gmail.com>
2025-05-30 21:30:49 -0500

    fix: use Link instead of a

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,16 +1,17 @@
+import Link from "next/link";
 import Nav from "./Nav";
 
 const Header = () => {
-   return (
-      <header className="absolute left-0 top-0  flex w-full items-center justify-between px-5 py-5 lg:px-7 lg:py-10">
-         <a href="/" aria-label="Logo Homero Bracamonte">
-            <abbr className=" font-dela text-2xl lg:text-4xl xl:text-5xl 2xl:text-6xl">
-               <span className="text-blue-500">H</span>B
-            </abbr>
-         </a>
-         <Nav />
-      </header>
-   );
+  return (
+    <header className="absolute left-0 top-0  flex w-full items-center justify-between px-5 py-5 lg:px-7 lg:py-10">
+      <Link href="/" aria-label="Logo Homero Bracamonte">
+        <abbr className=" font-dela text-2xl lg:text-4xl xl:text-5xl 2xl:text-6xl">
+          <span className="text-blue-500">H</span>B
+        </abbr>
+      </Link>
+      <Nav />
+    </header>
+  );
 };
 
 export default Header;
