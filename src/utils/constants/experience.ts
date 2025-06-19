2e571c83a9161687049206a50f8ad13e77bcc7e4
homero <homerojose69@gmail.com>
2025-06-19 15:34:55 -0500

    feat: update experience

--- src/utils/constants/experience.ts
+++ src/utils/constants/experience.ts
@@ -1,18 +1,18 @@
 import { ExperienceInfo } from "@/types";
 
 export const experience: ExperienceInfo[] = [
-   {
-      position: "Frontend Developer",
-      company: "300bees",
-      description:
-         "My journey in web development kickstarted back in 2022, where I dived headfirst into the world of website creation using WordPress, Elementor, and WooCommerce. This phase was all about mastering the art of building sleek and functional websites using HTML, CSS, and JavaScript. I also got hands-on experience managing a Shopify-based website, adding a valuable layer to my skill set and a deeper understanding of e-commerce platforms.",
-      date: "June 2022 - August 2023",
-   },
-   {
-      position: "Frontend Developer",
-      company: "Cressco",
-      description:
-         "Fast forward to 2023, and I've embraced a whole new world of modern technologies and frameworks. My focus now revolves around crafting robust solutions using React, React Query, Zustand, and TypeScript, among others. I've become a staunch advocate for code quality and maintainability, diving deep into testing methodologies with tools like Cypress and Jest. What's really exciting is my recent leap into mobile app development using React Native for iOS and Android platforms. This journey has been all about mastering best practices, honing my skills, and creating codebases that are not just functional but also maintainable and efficient.",
-      date: "August 2023 - Present",
-   },
+  {
+    position: "Frontend Developer",
+    company: "300bees",
+    description:
+      "Developed custom WordPress/WooCommerce websites with Elementor, HTML, CSS, and JavaScript. Managed Shopify stores, improving checkout UX and conversion rates. Optimized site speed and SEO for better search rankings.",
+    date: "June 2022 - August 2023",
+  },
+  {
+    position: "Frontend Developer",
+    company: "Cressco",
+    description:
+      "Developed cross-platform mobile apps with React Native (Expo) and web apps with React, TypeScript, and Next.js. Optimized performance using React Query, Zustand, and Prisma for state and data management. Built a food discovery app with social features (reviews, interactions) using React Native. Scale a Shopify + Gatsby e-commerce site for Josh Wood Colour, improving load time by 30%. Implemented automated testing with Jest, Cypress, and Detox for reliability. Collaborated on full-stack projects using Flask (Python) and Prisma for backend integration.",
+    date: "August 2023 - Present",
+  },
 ];
