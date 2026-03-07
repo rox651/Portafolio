43c92a1f2f1199b449ba0736bbed5ae33873f9b1
Homero <homerojose69@gmail.com>
2026-03-07 16:57:25 -0500

    fix: update experience

--- src/utils/constants/experience.ts
+++ src/utils/constants/experience.ts
@@ -12,7 +12,7 @@ export const experience: ExperienceInfo[] = [
     position: "Frontend Developer",
     company: "Cressco",
     description:
-      "Developed cross-platform mobile apps with React Native (Expo) and web apps with React, TypeScript, and Next.js. Optimized performance using React Query, Zustand, and Prisma for state and data management. Built a food discovery app with social features (reviews, interactions) using React Native. Scale a Shopify + Gatsby e-commerce site for Josh Wood Colour, improving load time by 30%. Implemented automated testing with Jest, Cypress, and Detox for reliability. Collaborated on full-stack projects using Flask (Python) and Prisma for backend integration.",
+      "Developed cross-platform mobile apps with React Native (Expo) and web apps with React, TypeScript, and Next.js. Optimized performance using React Query, Zustand, and Prisma for state and data management. Built a food discovery app with social features (reviews, interactions) using React Native. Scale a Shopify + Gatsby e-commerce site for Josh Wood Colour, improving load time by 30%. Implemented automated testing with Jest, Cypress, and Detox for reliability. Create a chatbot for an european bank using RAG strategy",
     date: "August 2023 - March 2026",
   },
   {
