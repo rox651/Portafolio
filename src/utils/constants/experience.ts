c491bea3dbb5bf5e892d60f53235fa341a06cfac
Homero <homerojose69@gmail.com>
2026-03-07 16:44:15 -0500

    feat: update my current experience

--- src/utils/constants/experience.ts
+++ src/utils/constants/experience.ts
@@ -13,6 +13,13 @@ export const experience: ExperienceInfo[] = [
     company: "Cressco",
     description:
       "Developed cross-platform mobile apps with React Native (Expo) and web apps with React, TypeScript, and Next.js. Optimized performance using React Query, Zustand, and Prisma for state and data management. Built a food discovery app with social features (reviews, interactions) using React Native. Scale a Shopify + Gatsby e-commerce site for Josh Wood Colour, improving load time by 30%. Implemented automated testing with Jest, Cypress, and Detox for reliability. Collaborated on full-stack projects using Flask (Python) and Prisma for backend integration.",
-    date: "August 2023 - Present",
+    date: "August 2023 - March 2026",
+  },
+  {
+    position: "Frontend Developer",
+    company: "COHNECTI",
+    description:
+      "I just started.So I am going to update my experience in the future 👀",
+    date: "March 2026 - Present",
   },
 ];
